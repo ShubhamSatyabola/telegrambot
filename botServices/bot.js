bc1dba57fc435d0b4044f0cd18aed35974a99863
ShubhamSatyabola <satyabolashubham@gmail.com>
2024-02-29 08:58:13 +0530

    comit

--- botServices/bot.js
+++ botServices/bot.js
@@ -1,5 +1,5 @@
 const Admin = require('../models/admin')
 exports.cronSchedule = async () => {
-    const admin = await Admin.findOne({}) // get the first
-    return admin.messageNumber
+    const admin = await Admin.find() // get the first
+    return admin[0].messageNumber
 };
